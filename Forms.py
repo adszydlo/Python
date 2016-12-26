39a98020a8a2538e86939746e91912d8f9700bdc
Adam Szydlo <adszydlo@o2.pl>
2016-12-26 20:53:28 +0100

    GeoDataAPI i mapa leaflef

--- Forms.py
+++ Forms.py
@@ -3,8 +3,18 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, Length
 
 class SignupForm(FlaskForm):
-  first_name = StringField('First name', validators=[DataRequired("Please enter your first name.")])
-  last_name = StringField('Last name', validators=[DataRequired("Please enter your last name.")])
-  email = StringField('Email', validators=[DataRequired("Please enter your email address."), Email("Please enter your email address.")])
-  password = PasswordField('Password', validators=[DataRequired("Please enter a password."), Length(min=6, message="Passwords must be 6 characters or more.")])
-  submit = SubmitField('Sign up')
+  first_name = StringField('Imię', validators=[DataRequired("Wpisz swoje imię.")])
+  last_name = StringField('Nazwisko', validators=[DataRequired("Wpisz swoje nazwisko.")])
+  email = StringField('Email', validators=[DataRequired("Wpisz swój adres email."), Email("Wpisz poprawny adres email.")])
+  password = PasswordField('Hasło', validators=[DataRequired("Wpisz swoje hasło."), Length(min=6, message="Hasło musi mieć conajmniej 6 znaków.")])
+  submit = SubmitField('Zarejestruj się')
+
+
+class LoginForm(FlaskForm):
+  email = StringField('Email', validators=[DataRequired("Wpisz swój adres email."), Email("Wpisz proszę poprawny adres email.")])
+  password = PasswordField('Hasło', validators=[DataRequired("Wpisz swoje hasło.")])
+  submit = SubmitField("Zaloguj się")
+
+class AddressForm(FlaskForm):
+  address = StringField('Adres', validators=[DataRequired("Podaj adres.")])
+  submit = SubmitField("Szukaj")
--- Models.py
+++ Models.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from urllib.request import urlopen, urljoin
+
+import geocoder
+import json
 
 db = SQLAlchemy()
 
@@ -23,3 +27,49 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+
+class Place(object):
+    def meters_to_walking_time(self, meters):
+        # powiedzmy ze predkosc chodu to 80m/min
+        return int(meters / 80)
+
+    def wiki_path(self, slug):
+        return urljoin("http://pl.wikipedia.org/wiki/", slug.replace(' ', '_'))
+
+    def address_to_latlng(self, address):
+        g = geocoder.google(address)
+        return (g.lat, g.lng)
+
+    def query(self, address):
+        lat, lng = self.address_to_latlng(address)
+
+        query_url = 'https://pl.wikipedia.org/w/api.php?action=query&list=geosearch&gsradius=5000&gscoord={0}%7C{1}&gslimit=20&format=json'.format(
+            lat, lng)
+        g = urlopen(query_url)
+        results = g.read().decode('utf8')
+        g.close()
+
+        data = json.loads(results)
+
+        places = []
+        for place in data['query']['geosearch']:
+            name = place['title']
+            meters = place['dist']
+            lat = place['lat']
+            lng = place['lon']
+
+            wiki_url = self.wiki_path(name)
+            walking_time = self.meters_to_walking_time(meters)
+
+            d = {
+                'name': name,
+                'url': wiki_url,
+                'time': walking_time,
+                'lat': lat,
+                'lng': lng
+            }
+
+            places.append(d)
+
+        return places
